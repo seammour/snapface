c1ad8223a34d722c2e6e4c43dda871dcc48a9e51
AMMOUR Salah Eddine <seammour@gmail.com>
2022-09-12 16:12:07 +0200

    completer les observable et eviter les fuite de mémoire

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
@@ -9,17 +9,27 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
+
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
     interval(1000).pipe(
+      take(10),
       tap(console.log)
     ).subscribe();
   }
+  
+
+  ngOnDestroy(): void {
+    
+  }
+
+
 
 }
